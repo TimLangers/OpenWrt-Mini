8ec2c5ede69c8ae89d097dbebaef01dfd4e46dae
TimLangers <yangcui2049@163.com>
2026-05-18 17:15:23 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 解释或添加 Feed 源
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# add feed lines
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 1. 强力克隆 OpenClash 官方最新主分支源码
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-# 添加 jerrykuku 的 Argon 最新主题源码
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# 2. 克隆 jerrykuku 的 Argon 最新精美主题及配置后台源码
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
